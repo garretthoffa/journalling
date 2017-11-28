8f71af9254570a802332499ee7888f7a72dd3a7e
Trevor Smith <tsmith1644@gmail.com>
2017-11-28 16:01:55 -0700

    Basis for top nav and javascript added

---
+++ resources/script.js
@@ -0,0 +1,7 @@
+function addNavigationBar() {
+    var topNav = document.getElementById("top-nav");
+    var topNavContent = "hello world!";
+    topNav.innerHTML = topNavContent;
+}
+
+addNavigationBar();
