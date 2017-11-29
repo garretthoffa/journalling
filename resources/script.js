6f4fb10f998b2677757ae53710e91ebe8659d21c
Trevor Smith <tsmith1644@gmail.com>
2017-11-28 17:11:16 -0700

    Fixed CSS paths

--- resources/script.js
+++ resources/script.js
@@ -1,12 +1,12 @@
-var rootURL = "http://ec2-18-221-5-177.us-east-2.compute.amazonaws.com/git/journalling/";
-//var rootURL = "file:///D:/tsmit/Documents/CS356/journalling/";
+//var rootURL = "http://ec2-18-221-5-177.us-east-2.compute.amazonaws.com/git/journalling/";
+var rootURL = "file:///D:/tsmit/Documents/CS356/journalling/";
 
 function addNavigationBar() {
     var topNav = document.getElementById("top-nav");
     var homeLink = "<li><a href=\"" + rootURL + "index.html\">Home</a></li>";
     var myJournalLink = "<li><a href=\"" + rootURL + "journal/summary.html\">My Journal</a></li>";
     var loginLink = "<li><a href=\"" + rootURL + "login.html\">Login</a></li>";
-    var topNavContent = "<ul>" + homeLink + myJournalLink + loginLink + "</ul>";
+    var topNavContent = "<ul id=\"top-nav-list\">" + homeLink + myJournalLink + loginLink + "</ul>";
     topNav.innerHTML = topNavContent;
 }
 
