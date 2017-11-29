e72ead1e88eed2cd658ae2cad0332ceb539c5054
Trevor Smith <tsmith1644@gmail.com>
2017-11-28 17:03:50 -0700

    Implemented Base of New entry button

--- resources/script.js
+++ resources/script.js
@@ -1,5 +1,5 @@
-var rootURL = "http://ec2-18-221-5-177.us-east-2.compute.amazonaws.com/git/journalling/";
-//var rootURL = "file:///D:/tsmit/Documents/CS356/journalling/";
+//var rootURL = "http://ec2-18-221-5-177.us-east-2.compute.amazonaws.com/git/journalling/";
+var rootURL = "file:///D:/tsmit/Documents/CS356/journalling/";
 
 function addNavigationBar() {
     var topNav = document.getElementById("top-nav");
@@ -9,6 +9,7 @@ function addNavigationBar() {
     var topNavContent = "<ul>" + homeLink + myJournalLink + loginLink + "</ul>";
     topNav.innerHTML = topNavContent;
 }
+
 function addFirebase(){
    var config = {
     apiKey: "AIzaSyCx28v6bZBNW2fq2mbPZl09tt5SZr2bFSg",
@@ -19,6 +20,10 @@ function addFirebase(){
     messagingSenderId: "296381439395"
   };
   firebase.initializeApp(config);
+}
+
+function createNewJournalEntry(){
+    window.location= rootURL + "journal/editor.html";
 }
 
 addFirebase();
