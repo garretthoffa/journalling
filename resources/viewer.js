9fda29c95010710c50acf610d4948368968c0a88
Trevor Smith <tsmith1644@gmail.com>
2017-12-07 10:25:52 -0700

    Hopefully fixed getting information

--- resources/viewer.js
+++ resources/viewer.js
@@ -14,9 +14,10 @@ function fillJournalEntry(){
             var userId = firebase.auth().currentUser.uid;
             firebase.database().ref('/journals/' + userId).once('value').then(function(snapshot) {
                 var entryId = getParameterByName("entry");
-                var date = snapshot[entryId].date;
-                var title = snapshot[entryId].title;
-                var entry = snapshot[entryId].entry;
+                var journalEntries = snapshot.val();
+                var date = journalEntries[entryId].date;
+                var title = journalEntries[entryId].title;
+                var entry = journalEntries[entryId].entry;
                 document.getElementById("date").innerHTML = date;
                 document.getElementById("title").innerHTML = title;
                 document.getElementById("entry").innerHTML = entry;
