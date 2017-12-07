69117099ddc7030aa517d8d8d5ffca96a8d270eb
Trevor Smith <tsmith1644@gmail.com>
2017-12-07 10:10:01 -0700

    Updated link to viewer to have entry uid

--- resources/entries.js
+++ resources/entries.js
@@ -1,3 +1,5 @@
+var rootURL = "http://ec2-18-221-5-177.us-east-2.compute.amazonaws.com:3006/journalling/";
+
 function getJournalTest(){
     firebase.auth().onAuthStateChanged(function(user){
         if(user){
@@ -6,7 +8,7 @@ function getJournalTest(){
                 snapshot.forEach(function(childSnapshot) {
                     var key = childSnapshot.key;
                     var childData = childSnapshot.val();
-                    var link = "<li>" + childData.date + " " + childData.title + "</li>"
+                    var link = "<li><a href=\"" + rootURL + "journal/editor.html?entry=" + key + "\">" + childData.date + " " + childData.title + "</a></li>"
                     document.getElementById("entry-list").innerHTML += link;                
                     console.log(childData);
                 });
